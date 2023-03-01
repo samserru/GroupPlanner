da273c596811a5826adbb1225886f874ef314745
Sam Serruya <sammyyrap7@gmail.com>
2023-03-01 09:20:46 -0500

    Initial Commit

---
+++ GroupPlanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GroupPlanner
+//
+//  Created by Samuel Serruya (student LM) on 3/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GroupPlanner/GroupPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupPlannerApp.swift
+//  GroupPlanner
+//
+//  Created by Samuel Serruya (student LM) on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GroupPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
